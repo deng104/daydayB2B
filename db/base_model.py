df0b05e50a3080acfd3ee3d9559c3f50c51f6cba
dengpeng <529722669@qq.com>
2019-09-24 15:49:37 +0800

    2019-9-24

--- db/base_model.py
+++ db/base_model.py
@@ -6,3 +6,7 @@ class BaseModel(models.Model):
     create_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
     update_time = models.DateTimeField(auto_now=True, verbose_name='更新时间')
     is_delete = models.BooleanField(default=False, verbose_name='删除标记')
+
+    class Meta:
+        # 说明是一个抽象模型类
+        abstract = True
