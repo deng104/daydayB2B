8912a7f9b3a450e12fca8307c2086fa405f640f0
dengpeng <529722669@qq.com>
2019-10-06 13:21:31 +0800

    2019-10-06

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from user import views
+from user.views import RegisterView
 
 urlpatterns = [
     url(r'^register$', views.register, name='register'),  # 注册
     url(r'^register_handle$', views.register_handle, name='register_handle'),  # 注册
+    url(r'^reg/$', RegisterView.as_view(), name='reg'),
 ]
--- apps/user/views.py
+++ apps/user/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse, redirect
 from django.core.urlresolvers import reverse
+from django.views.generic.base import View
 from user.models import User
 import re
 
@@ -33,3 +34,39 @@ def register(request):
         # 返回应答
         return redirect(reverse('goods:index'))
     return render(request, 'register.html')
+
+
+def register_handle(request):
+    """进行注册处理"""
+    pass
+
+
+class RegisterView(View):
+    """注册"""
+    def get(self, request):
+        # 显示注册页面
+        return render(request, 'register.html')
+    
+    def post(self, request):
+        username = request.POST.get('user_name')
+        password = request.POST.get('pwd')
+        email = request.POST.get('email')
+        allow = request.POST.get('allow')
+        if not all([username, password, email]):
+            return render(request, 'register.html', {'errmsg': '数据不完整'})
+        if not re.match(r'^[a-z0-9][\w.\-]*@[a-z0-9\-]+(\.[a-z]{2, 5}){1, 2}$', email):
+            return render(request, 'register.html', {'errmsg': '邮箱格式不正确'})
+        if allow != 'on':
+            return request(request, 'register.html', {'errmsg': '请同意协议'})
+        user = User.objects.filter(username=username)
+        if user:
+            return request(render, 'register.html', {'errmsg': '用户名已存在'})
+        
+        user = User.objects.create_user(username, email, password)
+        user.is_active = 0
+        user.save()
+        return 
+            
+            
+            
+    
